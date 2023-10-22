45975e4b607d04ce4d51ef000249f97826ff7318
Nebojsha <nebojsha.97.mitikj@gmail.com>
2023-10-22 23:42:24 +0200

    [main] Install Kinesis

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-
+  import { KinesisContainer, KinesisElement} from 'vue-kinesis';
 </script>
 
 <template>
+  <kinesis-container class="flex justify-center text-center items-center text-4xl text-white" style="height: 70vh">
+    <div>
 
+      <kinesis-element :strength="15">
+        <img class="border-[14px] border-emerald-500 mx-auto rounded-full w-48 h-48" src="src/assets/images/me.jpg" alt="Me">
+      </kinesis-element>
+      <kinesis-element :strength="17">
+        Hi, I'm Nebojsha 👋
+      </kinesis-element>
+      <kinesis-element :strength="19">
+        I'm a full-stack web developer
+      </kinesis-element>
+    </div>
+  </kinesis-container>
 </template>
