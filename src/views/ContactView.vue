15b90096720543b296fe92738f01d8643a102de4
Nebojsha <nebojsha.97.mitikj@gmail.com>
2023-10-26 21:05:57 +0200

    [main] Design about page and contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,63 @@
 <script setup lang="ts">
-
+import {KinesisContainer, KinesisElement} from 'vue-kinesis';
 </script>
 
 <template>
+  <kinesis-container class="sm:px-0 px-4 text-white max-w-xl mx-auto mb-14 mt-[4%]">
+    <h1 class="text-center sm:text-4xl mt-4 text-2xl">
+      Contact me
+    </h1>
+    <p class="mt-4 sm:text-lg">
+      If you’d like to chat about a project or just have question, please
+      fill in the form below. I aim to get back within 2 days. Software for an advertising agency,
+      a start-up, a student-led design studio, and a huge.
+    </p>
 
+    <div class="grid grid-cols-12 gap-x-2 gap-y-2 mt-4 text-sm">
+      <div class="col-span-6">
+        <label for="email" class="mb-1 block text-white">
+          Name
+        </label>
+        <input
+            type="text"
+            id="email" class="border rounded-lg w-full bg-zinc-800
+              border-zinc-600 text-white focus:ring-emerald-500 focus:border-emerald-500">
+      </div>
+      <div class="col-span-6">
+        <label class="mb-1 block text-white">
+          Email
+        </label>
+        <input
+            type="email"
+            class="border rounded-lg w-full bg-zinc-800
+              border-zinc-600 text-white focus:ring-emerald-500 focus:border-emerald-500">
+      </div>
+      <div class="col-span-12">
+        <label for="subject" class="mb-1 block text-white">
+          Subject
+        </label>
+        <input
+            type="text"
+            class="border rounded-lg w-full bg-zinc-800
+              border-zinc-600 text-white focus:ring-emerald-500 focus:border-emerald-500">
+      </div>
+
+      <div class="col-span-12">
+        <label for="subject" class="mb-1 block text-white">
+          Message
+        </label>
+        <textarea rows="8"
+                  class="resize-none w-full rounded-lg border bg-zinc-800 border-zinc-600 placeholder-zinc-400 text-white focus:ring-emerald-500 focus:border-emerald-500"></textarea>
+      </div>
+    </div>
+
+    <div class="text-center mt-5">
+      <kinesis-element class="text-2xl" :strength="5">
+        <button class="py-2 px-4 text-xl rounded-2xl text-white bg-emerald-600 hover:bg-emerald-700">
+          Send Message
+        </button>
+      </kinesis-element>
+    </div>
+
+  </kinesis-container>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,32 @@
 <script setup lang="ts">
-  import { KinesisContainer, KinesisElement} from 'vue-kinesis';
+import {KinesisContainer, KinesisElement} from 'vue-kinesis';
+import {RouterLink} from 'vue-router';
 </script>
 
 <template>
-  <kinesis-container class="flex justify-center text-center items-center text-4xl text-white" style="height: 70vh">
-    <div>
-
-      <kinesis-element :strength="15">
-        <img class="border-[14px] border-emerald-500 mx-auto rounded-full w-48 h-48" src="src/assets/images/me.jpg" alt="Me">
-      </kinesis-element>
-      <kinesis-element :strength="17">
-        Hi, I'm Nebojsha 👋
-      </kinesis-element>
-      <kinesis-element :strength="19">
-        I'm a full-stack web developer
-      </kinesis-element>
-    </div>
+  <kinesis-container class="sm:px-0 px-4 text-white max-w-xl mx-auto mb-14 mt-[4%]">
+    <img alt="Profile picture" src="src/assets/images/me.jpg" class="sm:w-32 w-24 mx-auto rounded">
+    <h1 class="text-center sm:text-4xl mt-4 text-2xl">
+      Welcome! I'm Nebojsha. <br> Full-stack web developer, based in Ohrid, Macedonia.
+    </h1>
+    <p class="mt-4 sm:text-lg">
+      Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first
+      into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the
+      privilege of building software for an advertising agency, a start-up, a student-led design
+      studio, and a huge corporation.
+    </p>
+    <p class="mt-4 sm:text-lg">
+      My main focus these days is building products and leading projects for our clients at Upstatement.
+      In my free time I've also released an online video course that covers everything you need to
+      know to build a web app with the Spotify API.
+    </p>
+    <kinesis-element class="text-2xl text-center mt-6" :strength="5">
+      <RouterLink
+          to="/experience"
+          class="py-2 px-4 text-xl rounded-2xl text-white bg-emerald-600 hover:bg-emerald-700"
+      >
+        Next 👉
+      </RouterLink>
+    </kinesis-element>
   </kinesis-container>
 </template>
